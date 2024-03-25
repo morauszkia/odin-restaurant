524f77c4af3b181550cec1bc10f3cef2e75f75c4
mantis <morauszki.andras@gmail.com>
2024-03-25 23:18:38 +0100

    basic file structure & home page hero hardcoding

--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
+import renderHome from './home';
+import renderMenu from './menu';
+import renderContact from './contact';
+
+const contentEl = document.getElementById('content');
+
 console.log('Hello at the restaurant page!');
---
+++ src/menu.js
@@ -0,0 +1,3 @@
+function render(container) {}
+
+export default render;
